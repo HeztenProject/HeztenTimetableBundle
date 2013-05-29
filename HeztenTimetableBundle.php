ae1f1b124590fd9779222b97a529194354bbd25d
Gorka Laucirica <gorka.lauzirika@gmail.com>
2013-05-29 20:55:06 +0200

    Fixed namespaces

---
+++ HeztenTimetableBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Hezten\HeztenTimetableBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle as BaseBundle;
+
+class HeztenTimetableBundle extends BaseBundle
+{
+}
--- TimetableBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Hezten\TimetableBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle as BaseBundle;
-
-class TimetableBundle extends BaseBundle
-{
-}
